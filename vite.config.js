226e3ba3dea53ac36572a8e24d5439c09aeffe6c
Rodey Luo <rodeyluo@gmail.com>
2026-04-24 18:20:40 +0800

    feat: add GitHub Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src')
     }
   },
-  base: './',
+  base: '/fastgpt-report/',
   optimizeDeps: {
     include: ['pdfjs-dist', 'mammoth', 'papaparse', 'xlsx', 'turndown', 'joplin-turndown-plugin-gfm']
   },
